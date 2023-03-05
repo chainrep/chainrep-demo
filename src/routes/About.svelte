e1ecaac2554e4c76d9051521d01ffc616ca27750
midpoint68 <trmidpoint@gmail.com>
2023-03-04 23:38:36 -0700

    Add about into

--- src/routes/About.svelte
+++ src/routes/About.svelte
@@ -1,4 +1,35 @@
 <h1>About</h1>
+<h3 style:font-weight="normal">
+  <strong>chainrep</strong> is an open-source protocol to democratize web3 security with open, onchain data.
+</h3>
+
 <p>
-  A public static SPA template using svelte.
+  Our goal is to improve web3 security for users by creating the most comprehensive dataset of scams and vulnerabilities. Since <strong>chainrep</strong> is an permissionless protocol, everyone can leverage and contribute to a shared source of information.
 </p>
+
+<h3 class="safu">
+  Let's make web3 <span>safu</span> together.
+</h3>
+
+<h2>How it Works</h2>
+<img class="diagram" src="img/diagram.png" alt="chainrep protocol diagram">
+
+<!-- Style -->
+<style>
+  img.diagram {
+    width: 800px;
+    max-width: 90%;
+    border-radius: var(--border-radius);
+  }
+
+  h3.safu {
+    color: var(--c0);
+    text-shadow: 2px 2px 0 var(--c1);
+  }
+
+  h3.safu > span {
+    color: var(--c3);
+    text-decoration: underline;
+    text-underline-offset: 3px;
+  }
+</style>
