1df7b91fd5c4012470bbb07292678697ef8d1649
midpoint68 <trmidpoint@gmail.com>
2023-03-04 22:02:45 -0700

    Add report component

--- src/components/Report.svelte
+++ src/components/Report.svelte
@@ -14,17 +14,40 @@
     <span class="id">#{reportId}</span>
     <span class="reviewer">{reviewer.slice(0, 6)}...{reviewer.slice(-4)}</span>
   </h3>
+  <div class="full-report">
+    <strong>Full Report:</strong>
+    <a href={uri} target="_blank" rel="noreferrer">{uri}</a>
+  </div>
+  <div>
+    <!-- Certs -->
+  </div>
 </div>
 
 <style>
   #report {
     padding: 1rem;
     border-radius: var(--border-radius);
-    background-color: var(--c4);
+    background-color: var(--c3);
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+    width: 600px;
+    max-width: 100%;
+    box-sizing: border-box;
+  }
 
+  #report > h3 {
+    margin: 0;
+    border-bottom: 1px solid var(--shadow-color);
   }
 
   #report > h3 > span.id {
     margin-right: 1rem;
+  }
+
+  .full-report {
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow-x: hidden;
   }
 </style>
--- src/routes/Search.svelte
+++ src/routes/Search.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import Report from "../components/Report.svelte";
+
   let searchStr: string;
 
   const search = async () => {
@@ -8,6 +10,14 @@
       console.error(err);
     }
   };
+  
+  const reports = [
+    {
+      reportId: 0,
+      reviewer: "0xa184aa8488908b43cCf43b5Ef13Ae528693Dfd00",
+      uri: "ipfs://bafkreieb5xpcpwatmqmm2eb6y2f72fx2yokapmrq75axqt3jdoc542dpd4"
+    }
+  ];
 </script>
 
 <h1>Search Reports</h1>
@@ -17,6 +27,14 @@
   <i class="icofont-search-2" />
 </div>
 
+<!-- Reports -->
+<div id="reports">
+  {#each reports as report}
+    <Report {...report} />
+  {/each}
+</div>
+
+<!-- Style -->
 <style>
   .search-bar {
     position: relative;
@@ -31,6 +49,7 @@
     font-weight: normal;
     max-width: 600px;
     flex-grow: 1;
+    box-sizing: border-box;
   }
 
   .search-bar > input:focus {
@@ -47,5 +66,9 @@
     top: 50%;
     font-size: large;
     transform: translateY(-50%);
+  }
+
+  #reports {
+    margin-top: 3rem;
   }
 </style>
