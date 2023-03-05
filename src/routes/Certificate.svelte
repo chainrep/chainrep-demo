4c83e69a18faf3b3a429dbd46c3078552f741124
midpoint68 <trmidpoint@gmail.com>
2023-03-05 02:45:01 -0700

    add cert functions

--- src/routes/Certificate.svelte
+++ src/routes/Certificate.svelte
@@ -2,33 +2,75 @@
   import type { ethers } from "ethers";
   import { pushNotification } from "../components/Notifications.svelte";
   import TextInput from "../components/TextInput.svelte";
-  import { signer } from "../utils/connection";
+  import { contract, signer } from "../utils/connection";
 
   let certName: string = "";
 
   let signerAddress: string | undefined;
-  $: $signer, updateCerts();
+  $: $signer, $contract, updateCerts();
   const updateCerts = async () => {
     if($signer) {
       signerAddress = await $signer.getAddress();
+      if($contract) {
+        const filter = $contract.filters.CreateCertificate(null, signerAddress);
+        const events = await $contract.queryFilter(filter);
+        myAuthorityCerts = events.map((e: any) => {
+          return {
+            id: e.args.certificateId,
+            name: e.args.name
+          };
+        })
+      } else {
+        myAuthorityCerts = [];
+      }
     } else {
       signerAddress = undefined;
     }
   };
 
   const createCert = async () => {
-    
+    try {
+      if(!$contract) return pushNotification({ message: "Not connected...", type: "warning" });
+      const tx = await $contract.createCertificate(certName);
+      pushNotification({ message: "Submitting transaction...", type: "standard" });
+      const receipt = await tx.wait();
+      pushNotification({ message: "Transaction submitted!", type: "success" });
+    } catch(err) {
+      console.error(err);
+      pushNotification({ message: "Failed to create certificate...", type: "error" });
+    }
   };
 
   let myAuthorityCerts: { id: ethers.BigNumberish, name: string }[] = [];
-  $: issueTo = myAuthorityCerts.map(() => "");
+  let issueTo: string[] = [];
+  $: myAuthorityCerts, resetIssueTo();
+  const resetIssueTo = () => issueTo = myAuthorityCerts.map(() => "");
+  const issueToProxy = (i: number) => issueTo[i];
 
-  const issue = async () => {
-
+  const issue = async (certificateId: ethers.BigNumberish, reviewer: string) => {
+    try {
+      if(!$contract) return pushNotification({ message: "Not connected...", type: "warning" });
+      const tx = await $contract.issueCertificate(certificateId, reviewer);
+      pushNotification({ message: "Submitting transaction...", type: "standard" });
+      const receipt = await tx.wait();
+      pushNotification({ message: "Transaction submitted!", type: "success" });
+    } catch(err) {
+      console.error(err);
+      pushNotification({ message: "Failed to issue certificate...", type: "error" });
+    }
   };
 
-  const revoke = async () => {
-    pushNotification({ message: "not implemented", type: "error" });
+  const revoke = async (certificateId: ethers.BigNumberish, reviewer: string) => {
+    try {
+      if(!$contract) return pushNotification({ message: "Not connected...", type: "warning" });
+      const tx = await $contract.revokeCertificate(certificateId, reviewer);
+      pushNotification({ message: "Submitting transaction...", type: "standard" });
+      const receipt = await tx.wait();
+      pushNotification({ message: "Transaction submitted!", type: "success" });
+    } catch(err) {
+      console.error(err);
+      pushNotification({ message: "Failed to revoke certificate...", type: "error" });
+    }
   };
 </script>
 
@@ -44,8 +86,8 @@
       <h3>{cert.name ?? `Cert #${cert.id}`}</h3>
       <TextInput label="Address" bind:value={issueTo[i]} placeholder="0x1234..." />
       <div>
-        <button on:click={issue}>Issue</button>
-        <button on:click={revoke}>Revoke</button>
+        <button on:click={() => issue(cert.id, issueToProxy(i))}>Issue</button>
+        <button on:click={() => revoke(cert.id, issueToProxy(i))}>Revoke</button>
       </div>
     </div>
   {/each}
@@ -54,7 +96,7 @@
 <!-- Style -->
 <style>
   .cert > h3 {
-    margin: 0;
+    margin: 0.5rem 0;
   }
 
   .cert {
